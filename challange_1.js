768207508db133dc88102b8d7b7d78654f7f08da
snarlee <snarlyoung@gmail.com>
2024-12-09 23:52:51 +0300

    Update challange_1.js

--- challange_1.js
+++ challange_1.js
@@ -1,32 +1,25 @@
- function calculateGrade( marks )
+//initialise calculation
+function calculateGrade( marks )
  {
 if ( marks>79 ){
     return 'A';
-}
-else if ( marks>=60 )
-    {
-return 'B';
 
-}
-else if ( marks>=50)
-    {
-return 'C';
-}
+}else if ( marks>=60 ){
+    return 'B';
 
-else if ( marks>=40)
-{
-    return 'D';}
+}else if ( marks>=50){
+    return 'C';
 
-
-else 
-{
+}else if ( marks>=40){
+    return 'D';
+}else {
     return'E';
 }
 
  }
 
- const userMarks=parseInt( prompt )( "Enter Student Marks (0-100:");
+ const userMarks=parseInt( prompt ( "Enter Student Marks (0-100:"));
 
  const grade=calculateGrade(userMarks);
 
- console.log( 'Grade: ${grade}' );
+ console.log( 'Grade:', $grade );
